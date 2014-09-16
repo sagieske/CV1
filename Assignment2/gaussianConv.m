596a72a3d80c43c3868955df53bbcf0a7e199084
Elise <koster.elise@gmail.com>
2014-09-16 18:43:25 +0200

    Prettier layout

--- Assignment2/gaussianConv.m
+++ Assignment2/gaussianConv.m
@@ -1,4 +1,3 @@
 function imOut = gaussianConv(image_path, sigma_x, sigma_y)
-im = im2double(rgb2gray(imread(image_path)));
-
-imOut = conv2(gaussian(sigma_x), gaussian(sigma_y), im);
+    im = im2double(rgb2gray(imread(image_path)));
+    imOut = conv2(gaussian(sigma_x), gaussian(sigma_y), im);
