88d0941b4fb35cb99d585ed956a661a899c4a478
Elise <koster.elise@gmail.com>
2014-09-16 18:45:33 +0200

    Fixed edges

--- Assignment2/gaussianConv.m
+++ Assignment2/gaussianConv.m
@@ -1,3 +1,3 @@
 function imOut = gaussianConv(image_path, sigma_x, sigma_y)
     im = im2double(rgb2gray(imread(image_path)));
-    imOut = conv2(gaussian(sigma_x), gaussian(sigma_y), im);
+    imOut = conv2(gaussian(sigma_x), gaussian(sigma_y), im, 'valid');
