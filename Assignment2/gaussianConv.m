52c1d22af95b14a8e02145f7f21ad31bce2d84c6
Elise <koster.elise@gmail.com>
2014-09-16 17:54:40 +0200

    Start of convolution

--- Assignment2/gaussianConv.m
+++ Assignment2/gaussianConv.m
@@ -1,5 +1,14 @@
 function imOut = gaussianConv(image_path, sigma_x, sigma_y)
-imread(image_path)
-sigma_x
-sigma_y
+im = imread(image_path);
+for i=1:size(im,1),
+    A(i,:) = conv2(double(im(i,:)), double(gaussian(sigma_x)));
+end 
+image(A);
+for j=1:size(im,2),
+    B(:,j) = conv2(double(im(:,j)), double(gaussian(sigma_y)));
 end
+%image(B);
+imOut = conv2(A,B);
+%image(imOut)
+
+%image(imOut)
