ddde2018bb1505175cf3a2fa39282faee3488b73
sagieske <Sharongieske@gmail.com>
2014-09-16 18:51:12 +0200

    Update gaussianConv.m

--- Assignment2/gaussianConv.m
+++ Assignment2/gaussianConv.m
@@ -1,3 +1,7 @@
 function imOut = gaussianConv(image_path, sigma_x, sigma_y)
     im = im2double(rgb2gray(imread(image_path)));
     imOut = conv2(gaussian(sigma_x), gaussian(sigma_y), im, 'valid');
+    
+    G = fspecial('gaussian', 5, sigma_x) ;
+    correctBlur = conv2(im, G,'same');
+    figure, imshow(correctBlur);
