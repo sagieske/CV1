f3dfdad42b57cbd6018abe6742c151c3a2f0572e
Elise <koster.elise@gmail.com>
2014-09-29 21:31:12 +0200

    Solving for v

--- Assignment3/calculate_opticalflowmatrix.m
+++ Assignment3/calculate_opticalflowmatrix.m
@@ -3,11 +3,12 @@ function v = calculate_opticalflowmatrix(im_x_region, im_y_region, im_t_region)
             im_x_region = reshape(im_x_region, numel(im_x_region), 1);
             im_y_region = reshape(im_y_region, numel(im_y_region), 1);
             im_t_region = reshape(im_t_region, numel(im_t_region), 1);
-
+            
+            %A is a matrix of x-derivative, y-derivative
             A= [im_x_region im_y_region];
-            %Create b
+            %b is a vector of the negative of the t-derivative
             b = -1.* im_t_region;
             %Calculate v
-            a = A' * A;
-            ab = A' * b;
-            v = inv(a) * ab;
+            ata = A' * A;
+            pseudo_inverse = inv(ata) * A';
+            v = pseudo_inverse * b;
