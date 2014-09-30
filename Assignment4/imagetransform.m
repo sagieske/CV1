5d80a78d3f25f06a4917ed7147dc367180cfc4ed
sagieske <sharongieske@gmail.com>
2014-09-30 17:50:00 +0200

    got x and y and their primes

--- Assignment4/imagetransform.m
+++ Assignment4/imagetransform.m
@@ -1,6 +1,25 @@
-function imagetransform(im1, im2)
+function imagetransform(im1, im2, N)
     im1 = im2single(rgb2gray(imread(im1)));
     im2 = im2single(rgb2gray(imread(im2)));
+    
+    %Detect interest points in each image. Characterize the local
+    %appearance of the regions around interest points 
     [frames1, desc1] = vl_sift(im1);
     [frames2, desc2] = vl_sift(im2);
-    [matches] = vl_ubcmatch(desc1, desc2);
+    %Set of supposed matches between region descriptors in each image
+    [matches] = vl_ubcmatch(desc1, desc2)
+    size(matches);
+    %Repeat N times
+    for i=1:N
+        for j=1:4
+            index1 = matches(1,j);
+            index2 = matches(2,j);
+            x = frames1(1,index1)
+            y = frames1(1,index1)
+            x_prime = frames2(1,index2)
+            y_prime = frames2(2,index2)
+        end
+        
+        disp('hello');
+    end
+    
