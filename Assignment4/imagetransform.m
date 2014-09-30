b14b47a10567c475d35e365d58e36d1a64af78f6
Elise <koster.elise@gmail.com>
2014-09-30 17:16:48 +0200

    Performs sift and gets matches using vlfeat

--- Assignment4/imagetransform.m
+++ Assignment4/imagetransform.m
@@ -1,5 +1,6 @@
 function imagetransform(im1, im2)
-    im1 = im2double(rgb2gray(imread(im1)));
-    im2 = im2double(rgb2gray(imread(im2)));
-    
-    [frames, desc] = sift(im1)
+    im1 = im2single(rgb2gray(imread(im1)));
+    im2 = im2single(rgb2gray(imread(im2)));
+    [frames1, desc1] = vl_sift(im1);
+    [frames2, desc2] = vl_sift(im2);
+    [matches] = vl_ubcmatch(desc1, desc2);
