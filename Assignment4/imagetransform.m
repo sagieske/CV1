63b060f4c6e001401bc0cdc3fa6bd40e0ef5a283
sagieske <sharongieske@gmail.com>
2014-09-30 18:12:12 +0200

    plot points

--- Assignment4/imagetransform.m
+++ Assignment4/imagetransform.m
@@ -11,15 +11,60 @@ function imagetransform(im1, im2, N)
     size(matches);
     %Repeat N times
     for i=1:N
+        A = []
+        b = []
+        %Create A and b of match point and add to overall A and b
         for j=1:4
             index1 = matches(1,j);
             index2 = matches(2,j);
-            x = frames1(1,index1)
-            y = frames1(1,index1)
-            x_prime = frames2(1,index2)
-            y_prime = frames2(2,index2)
+            x = frames1(1,index1);
+            y = frames1(2,index1);
+            x_prime = frames2(1,index2);
+            y_prime = frames2(2,index2);
+            A = [A; x y 0 0 1 0; 0 0 x y 0 1];
+            b = [b; x_prime; y_prime];
+        end
+        %Compute transformation vector
+        transformationvector = pinv(A) * b
+        %size(matches,2) 
+        info_forplots = zeros(size(matches,2), 6)
+        for m=1:size(matches,2)
+            index1 = matches(1,m)
+            x = frames1(1,index1);
+            y = frames1(2,index1);
+            A = [x y 0 0 1 0; 0 0 x y 0 1];
+            test = A * transformationvector
+            %Get true xprime yprime
+            index2 = matches(2,m)
+            x_prime_true = frames2(1,index2);
+            y_prime_true = frames2(2,index2);
+            info_forplots(m,:) = [x, y, test(1), test(2), x_prime_true,y_prime_true]
+            
+        end
+        size(info_forplots)
+        % plot correct x, y image 1
+        subplot(1,2,1);
+        caption = sprintf('FIRST IMAGE');
+        imshow(im1);
+        hold on;
+        plot(info_forplots(:,1),info_forplots(:,2), 'r.', 'MarkerSize', 10);
+        [xa1 ya1] = ds2nfu(info_forplots(:,1),info_forplots(:,2));
+
+
+        
+        
+        subplot(1,2,2);
+        caption = sprintf('CALCULATED PRIMES');
+        imshow(im2);
+        hold on;
+        plot(info_forplots(:,3),info_forplots(:,4), 'r.', 'MarkerSize', 10);
+        [xa2 ya2] = ds2nfu(info_forplots(:,3),info_forplots(:,4));
+        
+        
+        % draw the lines
+        for k=1:numel(xa1)
+            annotation('line',[xa1(k) xa2(k)],[ya1(k) ya2(k)],'color','r');
         end
         
-        disp('hello');
     end
     
