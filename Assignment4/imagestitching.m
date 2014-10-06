c85b26c410b412ad91d5ebd0d1a2ccfef9534986
Elise <koster.elise@gmail.com>
2014-10-06 13:31:25 +0200

    Start of stitch

--- Assignment4/imagestitching.m
+++ Assignment4/imagestitching.m
@@ -10,6 +10,22 @@ function imagestitching(im1, im2)
         im2 = im2single(rgb2gray(imread(im2)));
     end
     transformed_im = imtransform(im2, tform);
+    trans2 = imtransform(im1, tform);
+    [frames1, desc1] = vl_sift(im1);
+    [frames2, desc2] = vl_sift(transformed_im);
+    %Set of supposed matches between region descriptors in each image
+    [matches] = vl_ubcmatch(desc1, desc2)
+    frames1(matches(1,1))
+    frames2(matches(2,1))
+    for i=1:size(matches,1)
+        index1 = matches(1, i);
+        index2 = matches(2, i);
+        x = frames1(1, index1);
+        y = frames1(2, index1);
+        x2 = frames2(1, index2);
+        y2 = frames2(2, index2);
+        new_x = round(x - x2)
+    end
     figure,
     subplot(1,3,1);
     imshow(im1);
