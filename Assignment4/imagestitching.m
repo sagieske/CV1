20330d49a2d4624691dd63ea68a166e545902bea
sagieske <sharongieske@gmail.com>
2014-10-06 14:18:35 +0200

    stiched image, still black borders around it

--- Assignment4/imagestitching.m
+++ Assignment4/imagestitching.m
@@ -17,19 +17,58 @@ function imagestitching(im1, im2)
     [matches] = vl_ubcmatch(desc1, desc2)
     frames1(matches(1,1))
     frames2(matches(2,1))
-    for i=1:size(matches,1)
-        index1 = matches(1, i);
-        index2 = matches(2, i);
+
+    start_x_array = ones(1,size(matches,2));
+    start_y_array = ones(1,size(matches,2));
+    % Create 5 random match indices
+    for j=1:size(matches,2)
+        % Get random match
+        %j = round(rand * size(matches,1));
+        %if j < 1
+        %    j = 1;
+        %elseif j > size(matches,2)
+        %    j = size(matches,2);
+        %end
+         % Get index of first matches
+        index1 = matches(1, j);
+        index2 = matches(2, j);
+        % Get x,y coordinates that correspond in both images
         x = frames1(1, index1);
         y = frames1(2, index1);
         x2 = frames2(1, index2);
         y2 = frames2(2, index2);
-        new_x = round(x - x2)
+        % start x for image
+        new_x = round(x - x2);
+        new_y = round(y - y2);
+        start_x_array(j) = new_x;
+        start_y_array(j) = new_y;
     end
+    % x and y of highest frequency
+    y = mode(start_x_array)
+    x = mode(start_y_array)
+   
+    x_1 = size(im1, 1);
+    x_2 = size(transformed_im, 1);
+    x_length = max(x + x_2, x_1);
+    y_1 = size(im1, 2);
+    y_2 = size(transformed_im, 2);
+    y_length = max(y + y_2, y_1);
+
+    newimage = zeros(x_length, y_length);
+    newimage(1:size(im1,1), 1:size(im1,2)) = im1;
+    figure, imshow(newimage);
+    newimage(x:x_2+x-1, y:y_2+y-1) = transformed_im;
+    figure, imshow(newimage)
+    %imtest(x:size(im1,1)-x,y:size(im2,1)-y) = im2(1:size(im1,1)-x, 1,1:size(im1,2)-y)
+    %imshow(imtest)
+    
     figure,
-    subplot(1,3,1);
+    %subplot(1,3,1);
     imshow(im1);
-    subplot(1,3,2);
-    imshow(transformed_im);
-    subplot(1,3,3);
-    imshow(im2);
+    hold on;
+    plot(start_x_array, start_y_array, 'r.', 'MarkerSize', 40);
+    %hold off;
+    %subplot(1,3,2);
+    %imshow(transformed_im);
+    %subplot(1,3,3);
+    %imshow(im2);
